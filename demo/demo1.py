0a95f31e77ae5afb463badde9904038bfd07d21a
wangweiwei1997 <1472989408@qq.com>
2020-12-21 21:32:25 +0800

    12211231

---
+++ demo/demo1.py
@@ -0,0 +1,66 @@
+# -*-coding:UTF-8 -*-
+
+
+#变量
+name=10
+cate_name=20
+
+#赋值(赋什么打什么，以最后一次为准）
+chang=15
+kuan=5
+gao=3
+tiji=chang*kuan*gao
+print(tiji)
+tiji='wangwei'
+print(tiji)
+
+#运算符和表达式
+month_sal=1000+200
+print(month_sal)
+
+#   /小数点
+print(9/3)#结果3.0
+#   //整数
+print(9//3)#结果3
+print(10//3)#结果3
+#   %除数的余数
+print(10/3)#结果1；读法：10模3
+#   **（幂）
+print(5**3)#结果：125
+
+
+#if
+sal=4000
+if sal>2000:
+    print('超高')
+#and or    
+sal=4000
+comm=300
+if sal>2000 and comm>200:
+    print('很高')
+    
+sal=4000
+comm=300
+if sal>2000 or comm>200:
+    print('很高')
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- myexcel/demo.py
+++ myexcel/demo.py
@@ -1,1 +1,39 @@
 # -*-coding:UTF-8 -*-
+
+
+# 读多少行
+from xlrd import open_workbook
+bk=open_workbook(r'd:\emp.xls')
+sheet=bk.sheet_by_name('empinfo')
+print(sheet.nrows)
+
+# 读多少列
+print(sheet.ncols)
+
+#读某单元格（ANALYST）
+cell_value = sheet.cell_value(12,6) #下标从0开始
+print(cell_value)
+
+#读整个第X（7）行数据
+row_values = sheet.row_values(6,4)#4代表切割到哪里
+print(row_values)
+
+#读某列（姓名）数据
+col_values = sheet.col_values(5,5)
+print(col_values)
+
+
+#读整个列表数据
+
+
+#分装整个列表
+class MyExcel():
+
+    def __init__(self, workbook_name, sheet_name):
+        self.workbook_name = workbook_name
+        self.sheet_name = sheet_name
+
+    def __sheet(self):
+        bk = open_workbook(self.workbook_name) 
+        sheet = bk.sheet_by_name(self.sheet_name)
+        return sheet
