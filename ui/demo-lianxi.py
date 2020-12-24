6e4592d3b5f5bd12248538b8668f5b8e5a5c1957
wangweiwei1997 <1472989408@qq.com>
2020-12-24 15:03:43 +0800

    241505

---
+++ ui/demo-lianxi.py
@@ -0,0 +1,13 @@
+# -*-coding:UTF-8 -*-
+from selenium import webdriver
+driver = webdriver.Chrome('..\drivers\chromedriver.exe')
+driver.get('http://192.168.1.4/ecshop/admin/privilege.php?act=login')
+driver.maximize_window()
+driver.find_element_by_name('username').send_keys('caichang')
+driver.find_element_by_name('password').send_keys('caichang1')
+driver.find_element_by_class_name('btn-a').click()
+sleep(1)
+
+driver.switch_to.frame('menu-frame')
+driver.find_element_by_link_text('商品列表').click()
+driver.switch_to.default_content()
--- ui/demo2-shangjia.py
+++ ui/demo2-shangjia.py
@@ -1,14 +1,23 @@
 # -*-coding:UTF-8 -*-
 from selenium import webdriver
-from time import sleep
-from ui.demo import driver
+
+
 
 #业务（谷歌）
-wb=webdriver.Chrome
+wb=webdriver.Chrome('..\drivers\chromedriver.exe')
 driver.get('http://192.168.1.4/ecshop/admin/privilege.php?act=login')
 driver.maximize_window()
 driver.find_elements_by_name('username').send_keys('caichang')
 driver.find_element_by_name('password').send_keys('caichang1')
 driver.find_element_by_class_name('btn-a').click()
-sleep(1)
+
 driver.switch_to.frame('menu-frame')
+driver.find_elements_by_link_text('商品列表').click()
+#退出，进另外一个
+driver.switch_to_default_content()
+driver.switch_to.frame('main-frame')
+
+good_names = driver.find_element_by_xpath('//*[@id="listDiv"]/table[1]/tbody/tr[3]/td[2]/span').text
+img_type = driver.find_element_by_xpath('//*[@id="listDiv"]/table[1]/tbody/tr[3]/td[2]/span').get_attribute('src').split('/')
+
+
